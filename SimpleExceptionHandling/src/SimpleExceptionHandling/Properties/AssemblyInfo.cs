ef4ffa558b56855c50779deb52c4b7359f932781
João Simões <nogravity00@outlook.com>
2016-05-16 18:36:01 +0100

    Release 1.0.0-alpha02

--- SimpleExceptionHandling/src/SimpleExceptionHandling/Properties/AssemblyInfo.cs
+++ SimpleExceptionHandling/src/SimpleExceptionHandling/Properties/AssemblyInfo.cs
@@ -51,4 +51,4 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(true)]
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha01")]
+[assembly: AssemblyInformationalVersion("1.0.0-alpha02")]
