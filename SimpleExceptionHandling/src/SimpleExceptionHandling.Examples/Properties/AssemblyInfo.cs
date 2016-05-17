bc823dbfc896d7b1be852c3f3169128f268e254f
João Simões <nogravity00@outlook.com>
2016-05-17 13:28:03 +0100

    Updated project properties

--- SimpleExceptionHandling/src/SimpleExceptionHandling.Examples/Properties/AssemblyInfo.cs
+++ SimpleExceptionHandling/src/SimpleExceptionHandling.Examples/Properties/AssemblyInfo.cs
@@ -1,23 +1,50 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿#region License
+// The MIT License (MIT)
+// 
+// Copyright (c) 2016 João Simões
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+#endregion
+using System;
+using System.Reflection;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+#if NET40 || NET45
+using System.Runtime.InteropServices;
+#endif
+
 [assembly: AssemblyTitle("SimpleExceptionHandling.Examples")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library that helps developers to handle exceptions outside catch blocks.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleExceptionHandling.Examples")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Net.JoaoSimoes")]
+[assembly: AssemblyProduct("SimpleExceptionHandling")]
+[assembly: AssemblyCopyright("Copyright © 2016 João Simões")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
+#if NET40 || NET45
+
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("012218d6-2181-4885-bb01-4173152f1c41")]
+
+#endif
+
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("1.0.*")]
