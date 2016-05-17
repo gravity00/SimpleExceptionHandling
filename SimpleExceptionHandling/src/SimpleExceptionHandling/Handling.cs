46f955d2d2b25ef2ff05b2d742a9a2d313edd464
João Simões <nogravity00@outlook.com>
2016-05-17 01:53:29 +0100

    Removed unexistent overloads

--- SimpleExceptionHandling/src/SimpleExceptionHandling/Handling.cs
+++ SimpleExceptionHandling/src/SimpleExceptionHandling/Handling.cs
@@ -41,40 +41,6 @@ namespace SimpleExceptionHandling
         /// <returns>The handling configuration</returns>
         /// <exception cref="ArgumentNullException"></exception>
         public static IHandlingConfiguration On<TException>(
-            Action<TException, IHandlingResult> handler, Func<TException, IHandlingResult, bool> condition = null)
-            where TException : Exception
-        {
-            if (handler == null) throw new ArgumentNullException(nameof(handler));
-
-            return new HandlingConfiguration().On(handler, condition);
-        }
-
-        /// <summary>
-        /// Creates a new <see cref="IHandlingConfiguration"/> instance with the given handler.
-        /// </summary>
-        /// <typeparam name="TException">The exception type</typeparam>
-        /// <param name="handler">The handler to be added</param>
-        /// <param name="condition">An optional condition to be checked if the handler must be used</param>
-        /// <returns>The handling configuration</returns>
-        /// <exception cref="ArgumentNullException"></exception>
-        public static IHandlingConfiguration On<TException>(
-            Action<TException> handler, Func<TException, IHandlingResult, bool> condition = null)
-            where TException : Exception
-        {
-            if (handler == null) throw new ArgumentNullException(nameof(handler));
-
-            return new HandlingConfiguration().On(handler, condition);
-        }
-
-        /// <summary>
-        /// Creates a new <see cref="IHandlingConfiguration"/> instance with the given handler.
-        /// </summary>
-        /// <typeparam name="TException">The exception type</typeparam>
-        /// <param name="handler">The handler to be added</param>
-        /// <param name="condition">An optional condition to be checked if the handler must be used</param>
-        /// <returns>The handling configuration</returns>
-        /// <exception cref="ArgumentNullException"></exception>
-        public static IHandlingConfiguration On<TException>(
             Action<TException, IHandlingResult> handler, Func<TException, bool> condition = null)
             where TException : Exception
         {
